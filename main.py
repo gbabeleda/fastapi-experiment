bb7e8d462b2d006ea8fda94562fd54fec0480819
Gio Abeleda <gioabeleda@gmail.com>
2025-06-07 04:43:52 +0800

    FastAPI Hello World

--- main.py
+++ main.py
@@ -1,6 +1,12 @@
-def main():
-    print("Hello from fastapi-experiment!")
+from fastapi import FastAPI
+
+app = FastAPI()
+
+
+@app.get("/")
+def read_roof():
+    return {"message": "Hello World"}
 
 
 if __name__ == "__main__":
-    main()
+    ...
